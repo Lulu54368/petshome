6ef326d175d8af8a60fa37fc8b93be42ba757f73
Lulu <yuntaol@student.unimelb.edu.au>
2022-08-22 17:29:43 +1000

    initialize

---
+++ src/main/java/com/itproject/petshome/PetshomeApplication.java
@@ -0,0 +1,13 @@
+package com.itproject.petshome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetshomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetshomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itproject/petshome/PetshomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itproject.petshome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetshomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
