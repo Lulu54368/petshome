43d4114c97312fdbc944aa3b9c5acf5dc9a4b76c
Lulu <yuntaol@student.unimelb.edu.au>
2022-09-18 22:01:23 +1000

    SES

--- src/main/java/com/itproject/petshome/service/EmailVerificationServiceImpl.java
+++ src/main/java/com/itproject/petshome/service/EmailVerificationServiceImpl.java
@@ -1,5 +1,9 @@
 package com.itproject.petshome.service;
 
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
+import com.amazonaws.services.simpleemail.model.*;
 import com.itproject.petshome.exception.UserCodeNotFoundException;
 import com.itproject.petshome.model.User;
 import com.itproject.petshome.repository.UserCodeRepository;
@@ -13,6 +17,8 @@ import org.springframework.stereotype.Service;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
+
+import static com.sun.mail.imap.SortTerm.TO;
 
 @Service
 @Setter
@@ -29,39 +35,46 @@ public class EmailVerificationServiceImpl implements EmailService {
     @Override
     public void sendEmail(User user, String siteURL) throws MessagingException, UserCodeNotFoundException {
 
-        String toAddress = user.getEmail();
+        final String FROM = "petshome@cute-lulu.com";
 
-        String subject = "Please verify your registration";
-        String content = "Hi there,"
-                + "<br>Please click the link below to verify your registration:<br>"
-                + "<h3><a href=\"[[URL]]\" target=\"_self\">VERIFY</a></h3>"
-                + "Thank you,<br>"
-                + "pet's home.co";
 
-        MimeMessage message = sender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(message);
+
+
+        // The subject line for the email.
+        final String SUBJECT = "Verify your email";
+
+        String TO = user.getEmail();
+
+        // The email body for recipients with non-HTML email clients.
+        final String TEXTBODY = "Please verify your email"+siteURL;
+
 
 
         try {
-            helper.setFrom("1245637623@qq.com");
-            helper.setTo(toAddress);
-            helper.setSubject(subject);
-            helper.setText(content);
-        } catch (MessagingException e) {
-            e.printStackTrace();
+                AmazonSimpleEmailService client =
+                        AmazonSimpleEmailServiceClientBuilder.standard()
+                                // Replace US_WEST_2 with the AWS Region you're using for
+                                // Amazon SES.
+                                .withRegion(Regions.AP_SOUTHEAST_2).build();
+                SendEmailRequest request = new SendEmailRequest()
+                        .withDestination(
+                                new Destination().withToAddresses(TO))
+                        .withMessage(new Message()
+                                .withBody(new Body()
+
+                                        .withText(new Content()
+                                                .withCharset("UTF-8").withData(TEXTBODY)))
+                                .withSubject(new Content()
+                                        .withCharset("UTF-8").withData(SUBJECT)))
+                        .withSource(FROM);
+                client.sendEmail(request);
+                System.out.println("Email sent!");
+            } catch (Exception ex) {
+                System.out.println("The email was not sent. Error message: "
+                        + ex.getMessage());
+            }
         }
-        String userCode = userCodeRepository
-                .findById(user.getId())
-                .getVerificationCode();
-        String verifyURL = siteURL + "/api/v1/auth/verify?code=" + userCode;
 
-        content = content.replace("[[URL]]", verifyURL);
-
-        helper.setText(content);
-
-
-        sender.send(message);
-    }
 
 
 
