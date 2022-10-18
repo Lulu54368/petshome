2a367497f2acbd905fbc89c3ea10fa90430c4a06
Lulu <yuntaol@student.unimelb.edu.au>
2022-10-19 09:07:15 +1100

    cross origin

--- src/main/java/com/itproject/petshome/config/ApplicationWebMvcConfigurer.java
+++ src/main/java/com/itproject/petshome/config/ApplicationWebMvcConfigurer.java
@@ -2,6 +2,7 @@ package com.itproject.petshome.config;
 
 import lombok.AllArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -9,12 +10,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Component
 @AllArgsConstructor
+@Configuration
 public class ApplicationWebMvcConfigurer implements WebMvcConfigurer {
 
     final ApplicationProperties applicationProperties;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/").allowedOrigins(applicationProperties.getAllowedOrigins());
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000");
     }
 }
--- src/main/java/com/itproject/petshome/config/SecurityConfig.java
+++ src/main/java/com/itproject/petshome/config/SecurityConfig.java
@@ -2,45 +2,22 @@ package com.itproject.petshome.config;
 
 import com.itproject.petshome.filter.AdminFilter;
 import com.itproject.petshome.filter.AuthoritiesLoggingAfterFilter;
-import com.itproject.petshome.filter.CorsFilter;
 import com.itproject.petshome.filter.JwtTokenFilter;
 import com.itproject.petshome.service.AdminService;
-import com.itproject.petshome.service.UserService;
 import de.codecentric.boot.admin.server.config.AdminServerProperties;
-import io.netty.handler.ssl.SslContextBuilder;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.reactive.ClientHttpConnector;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import reactor.netty.http.client.HttpClient;
 
-import javax.servlet.Filter;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import java.util.UUID;
 @Data
 @Configuration
@@ -56,7 +33,7 @@ public class SecurityConfig {
     private final PasswordEncoder passwordEncoder;
     private final AdminService adminService;
     private final JwtTokenFilter jwtTokenFilter;
-    private final CorsFilter corsFilter;
+    //private final CorsFilter corsFilter;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http)  throws Exception {
 
@@ -64,26 +41,17 @@ public class SecurityConfig {
 
 
             http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                    .cors().configurationSource(request -> {
-                        CorsConfiguration configuration = new CorsConfiguration();
-                        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
-                        configuration.setAllowedMethods(Arrays.asList("*"));
-                        configuration.setAllowCredentials(true);
-                        configuration.setAllowedHeaders(Collections.singletonList("*"));
-
-                        return configuration;
-
-                    }).and().csrf().disable()
+                    .cors().and().csrf().disable()
                     .addFilterAfter(jwtTokenFilter,
                             UsernamePasswordAuthenticationFilter.class)
                     .addFilterAfter(adminFilter, UsernamePasswordAuthenticationFilter.class)
                     .addFilterAfter(authoritiesLoggingAfterFilter, UsernamePasswordAuthenticationFilter.class)
-                    .addFilterAfter(corsFilter, UsernamePasswordAuthenticationFilter.class)
+                    //.addFilterAfter(corsFilter, UsernamePasswordAuthenticationFilter.class)
                     .authorizeHttpRequests((auth) -> {
                                 try {
                                     auth
                                             .antMatchers("/api/vi/user/*").authenticated()
-                                            .antMatchers("/api/v1/admin/*").authenticated()
+                                            .antMatchers("/api/v1/admin/*").permitAll()
 
                                             .antMatchers("/api/vi/**").permitAll()
                                             .and()
