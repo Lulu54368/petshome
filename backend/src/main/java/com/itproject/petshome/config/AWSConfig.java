cb4e79fd8eb3cb2dda1a195a8e342d9eb0dcbe72
Lulu <yuntaol@student.unimelb.edu.au>
2023-03-13 12:48:08 +1100

    aws credentials

--- backend/src/main/java/com/itproject/petshome/config/AWSConfig.java
+++ backend/src/main/java/com/itproject/petshome/config/AWSConfig.java
@@ -1,19 +1,28 @@
 package com.itproject.petshome.config;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.BasicSessionCredentials;
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+@AllArgsConstructor
 @Configuration
 public class AWSConfig {
+    private AWSProperties awsProperties;
+
     @Bean
     public AmazonS3  s3Client(){
+        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(
+                awsProperties.getS3_access_key(),
+                awsProperties.getS3_secret_key());
         Regions clientRegion = Regions.AP_SOUTHEAST_2;
         return AmazonS3ClientBuilder.standard()
                 .withRegion(clientRegion)
-                .withCredentials(new DefaultAWSCredentialsProviderChain())
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
                 .build();
     }
 
--- backend/src/main/java/com/itproject/petshome/config/AWSProperties.java
+++ backend/src/main/java/com/itproject/petshome/config/AWSProperties.java
@@ -1,13 +1,23 @@
 package com.itproject.petshome.config;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import lombok.Data;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties
 @Data
 public class AWSProperties {
+
+    Dotenv dotenv = Dotenv
+            .configure()
+            .directory("backend/")
+            .load();
     @Value("${aws.s3.bucket.name}")
     private String bucketName;
+    private String s3_access_key = dotenv.get("AWS_S3_ACCESS_KEY");
+
+    private String s3_secret_key = dotenv.get("AWS_S3_SECRET_KEY");
 }
